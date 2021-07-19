ee20069339f3b1851fc01136aa156088e1d7635a
Suryakanti003 <68381666+Suryakanti003@users.noreply.github.com>
2021-07-19 16:50:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,18 @@
+seats = [];
+
+
 function setup(){
   createCanvas(windowWidth,windowHeight);
+  
+  for(let i =0;i<4;i++){
+    let seat = new Seat(i*120+50, 400);
+    seats.push(seat);
+  }
 }
 
 function draw(){
-
+  for(let i =0;i<seats.length;i++){
+    seats[i].display();
+  }
 
 }

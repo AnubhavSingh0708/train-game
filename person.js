ba5a6199fdfe2c334351fa50c0828f2f6e204b87
Suryakanti003 <68381666+Suryakanti003@users.noreply.github.com>
2021-07-20 12:23:18 +0530

    Update person.js

--- person.js
+++ person.js
@@ -2,30 +2,29 @@ class Person{
 
   constructor(type,x ,y){
     this.x = x;
-    this.y=  y;
+    this.y = y;
     this.type = type;
   
   
   }
   
   display(){
- 		rect(this.x,this.y,25, 50);
+ 		rect(this.x, this.y, 25, 50);
   }
   
   move(){
-    if(dist(mouseX,mouseY,this.x,this.y)<25&&mouseIsPressed){
-    	this.x=mouseX;
-      this.y=mouseY;
+    if(dist(mouseX, mouseY, this.x, this.y) < 25 && mouseIsPressed){
+    	this.x = mouseX;
+      this.y = mouseY;
     }
   }
   
   snap(){
     for(let i =0;i<seats.length;i++){
-      if(dist(this.x,this.y,seats[i].x,seats[i].y)<50){
-				this.x=seats[i].x;
-				this.y=seats[i].y;
+      if(dist(this.x, this.y, seats[i].x, seats[i].y) < 50 && !mouseIsPressed){
+				this.x = seats[i].x;
+				this.y = seats[i].y;
 			}
     }
-  
   }
 }

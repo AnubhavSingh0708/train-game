012fab4ed7b92080bd2b445d5618120bd4478189
Suryakanti003 <68381666+Suryakanti003@users.noreply.github.com>
2021-07-20 12:04:46 +0530

    Update person.js

--- person.js
+++ person.js
@@ -9,13 +9,23 @@ class Person{
   }
   
   display(){
-   rect(this.x,this.y,25, 50);
+ 		rect(this.x,this.y,25, 50);
   }
   
   move(){
-    if(dist(mouseX,mouseY,this.x,this.y)<25){
-        this.x=mouseX;
-        this.y=mouseY;
+    if(dist(mouseX,mouseY,this.x,this.y)<25&&mouseIsDown){
+    	this.x=mouseX;
+      this.y=mouseY;
     }
+  }
+  
+  snap(){
+    for(let i =0;i<seats.length;i++){
+      if(dist(this.x,this.y,seats[i].x,seats[i].y)<50){
+				this.x=seats[i].x;
+				this.y=seats[i].y;
+			}
+    }
+  
   }
 }

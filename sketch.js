4c40736eedb196701d5799c04ee4ae83caf8106b
Suryakanti003 <68381666+Suryakanti003@users.noreply.github.com>
2021-07-20 11:40:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let seats = [];
-
+let persons = [];
 
 function setup(){
   createCanvas(windowWidth,windowHeight);
@@ -8,11 +8,17 @@ function setup(){
     let seat = new Seat(i*120+50, 400);
     seats.push(seat);
   }
+  
+  let person = Person("man", 1000, 500);
 }
 
 function draw(){
   for(let i =0;i<seats.length;i++){
     seats[i].display();
   }
-
+  
+  for(let i =0;i<persons.length;i++){
+    persons[i].display();
+    persons[i].snap();
+  }
 }

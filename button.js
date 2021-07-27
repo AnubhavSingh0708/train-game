2c2c31071b8925b285f46bb68dd0086dadb31501
Suryakanti003 <68381666+Suryakanti003@users.noreply.github.com>
2021-07-27 09:54:38 +0530

    Create button.js

---
+++ button.js
@@ -0,0 +1,18 @@
+class Button{
+
+  constructor(x,y,width,height,func){
+    this.pos=createVector(x,y);
+    this,width= width;
+    this.height=height;
+    this.func = func;
+  }
+
+  display(){
+    rect(this.pos.x,this.pos.y,this.width,this.height);
+  }
+
+  work(){
+    this.func()
+  }
+  
+}
